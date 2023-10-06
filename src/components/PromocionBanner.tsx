72460f1ca3cbaed179d265fa1cf480dbbc75fd44
BuildTools <gianputallaz@gmail.com>
2023-10-06 19:35:17 -0300

    added footer

--- src/components/PromocionBanner.tsx
+++ src/components/PromocionBanner.tsx
@@ -1,7 +1,7 @@
 export default function PromocionBanner() {
     return (
         <div className="my-8 flex justify-center">
-            <img className="border border-red-400" alt="promocion" src="Promocion.png" />
+            <img alt="promocion" src="Promocion.png" />
         </div>
     )
 }
