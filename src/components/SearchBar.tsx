0db1d5f694470fd755e8e4d3634fdb3135e9340b
Gian Putallaz <gianputallaz@gmail.com>
2023-10-19 20:39:21 -0300

    changed navbar design

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,9 @@
 export default function SearchBar() {
     return (
-        <div className="order-2 flex w-full items-center rounded-3xl bg-[#D9D9D9] px-2 md:order-none md:w-[30rem]">
+        <div className="order-2 flex w-full items-center rounded-3xl border-2 border-black bg-white px-2 md:order-none md:w-[30rem]">
             <img className="px-3 py-3" alt="search" src="SearchIcon.svg" />
             <input
-                className="h-max w-full rounded-r-3xl bg-[#D9D9D9] text-base placeholder-neutral-400 outline-none "
+                className="h-max w-full rounded-r-3xl bg-white text-base placeholder-neutral-400 outline-none "
                 type="text"
                 placeholder="Buscar por Titulo, Autor"
             />
