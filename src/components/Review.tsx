daac5394b4a9de46edb3922ecb8232829b9d4917
Gian Putallaz <gianputallaz@gmail.com>
2024-02-25 23:13:33 -0300

    se agrega la opcion de eliminar reseñas y valida la id del ejemplar en la pagina del libro

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,10 +1,15 @@
+import { userType } from '@/types/user'
+import DeleteReviewButton from './DeleteReviewButton'
+
 interface ReviewProps {
-    user: string
+    reviewUser: { idUsuario: string; nombre: string; apellido: string }
     score: number
     text: string
+    user: userType | undefined
+    idLibro: number
 }
 
-export default function Review({ user, score, text }: ReviewProps) {
+export default function Review({ reviewUser, score, text, user, idLibro }: ReviewProps) {
     return (
         <div className="flex flex-col gap-2 xl:w-[700px]">
             <div className="flex gap-2">
@@ -12,8 +17,9 @@ export default function Review({ user, score, text }: ReviewProps) {
                     <img src="/Star_lleno.svg" className="w-5" key={idx} />
                 ))}
             </div>
-            <p className="text-sm font-medium text-neutral-700">{user}</p>
+            <p className="text-sm font-medium text-neutral-700">{`${reviewUser.nombre} ${reviewUser.apellido}`}</p>
             <p className="font-medium">{text}</p>
+            <DeleteReviewButton user={user} reviewUser={reviewUser} idLibro={idLibro} />
         </div>
     )
 }
