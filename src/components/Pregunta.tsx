80320d4250b4c0e1ea8779d811a73a13874f1fef
Cristian Chivisky <cristian.chivisky@gmail.com>
2024-03-05 20:04:10 -0300

    Cambios en preguntas frecuentes

--- src/components/Pregunta.tsx
+++ src/components/Pregunta.tsx
@@ -3,13 +3,17 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './
 interface PreguntaProps {
     pregunta: string
     respuesta: string
+    onEliminar: () => void;
 }
-export default function Pregunta({ pregunta, respuesta }: PreguntaProps) {
+export default function Pregunta({ pregunta, respuesta, onEliminar }: PreguntaProps) {
     return (
         <Accordion type="single" collapsible>
             <AccordionItem value="item-1" className="border border-black px-8">
                 <AccordionTrigger className="font-semibold">{pregunta}</AccordionTrigger>
-                <AccordionContent>{respuesta}</AccordionContent>
+                <AccordionContent>
+                    {respuesta}
+                    <button onClick={onEliminar}  className="text-red-500 hover:text-red-600 font-bold px-4 py-2 rounded-md transition-all duration-300 ml-auto border-none bg-none ml-auto">Eliminar</button>
+                </AccordionContent>
             </AccordionItem>
         </Accordion>
     )
